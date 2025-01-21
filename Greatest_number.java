10dfcf46e859e7f6838c59be7beb77854cd8a36d
Avanti Kumari A <166237907+AvantiKumari-A@users.noreply.github.com>
2025-01-22 00:10:20 +0530

    Add files via upload

---
+++ Greatest_number.java
@@ -0,0 +1,22 @@
+//Find greatest among three numbers (a,b,c)
+
+import java.util.Scanner;
+class Greatest_number{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the three numbers: ");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+if(a>b)
+{
+System.out.println("A is the greatest number");
+}
+else if(b>c){
+System.out.println("B is greatest number");
+}
+else{
+System.out.println("C is greatest number");
+}
+}
+}
