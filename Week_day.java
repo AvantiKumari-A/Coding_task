2f00238c03e8f3b8576ea283d535b5530d95965e
Avanti Kumari A <166237907+AvantiKumari-A@users.noreply.github.com>
2025-01-22 00:10:54 +0530

    Add files via upload

---
+++ Week_day.java
@@ -0,0 +1,37 @@
+// Display Weekday name from the input as int
+
+import java.util.Scanner;
+class Week_day{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number(1-7): ");
+int day=sc.nextInt();
+switch(day){
+case 1:
+System.out.println("Today is Monady");
+break;
+case 2:
+System.out.println("Today is Tuesday");
+break;
+case 3:
+System.out.println("Today is Wednesday");
+break;
+case 4:
+System.out.println("Today is Thursday");
+break;
+case 5:
+System.out.println("Today is Friday");
+break;
+case 6:
+System.out.println("Today is Saturday");
+break;
+case 7:
+System.out.println("Today is Sunday");
+break;
+default:
+System.out.println("Oops!! Enter the correct number");
+break;
+}
+}
+}
+
