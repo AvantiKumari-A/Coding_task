451d0616694e5e0cd9e761a644545f5c384034c5
Avanti Kumari A <166237907+AvantiKumari-A@users.noreply.github.com>
2025-01-22 21:57:58 +0530

    Add files via upload

---
+++ Natural_Number_Sum.java
@@ -0,0 +1,12 @@
+//Natural numbers and their sum
+
+import java.util.Scanner;
+class Natural_Number_Sum{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the n value of natural number: ");
+int n=sc.nextInt();
+int sum=(n*(n+1))/2;
+System.out.println("Sum of "+ n + " natural numbers is: " + sum); 
+}
+}
