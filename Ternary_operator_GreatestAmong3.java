d6f0536342b5521f64ab6ead5eec42cf72704fcf
Avanti Kumari A <166237907+AvantiKumari-A@users.noreply.github.com>
2025-01-22 23:00:28 +0530

    Add files via upload

---
+++ Ternary_operator_GreatestAmong3.java
@@ -0,0 +1,14 @@
+//to find greatest among 3 using ternary operator
+
+import java.util.Scanner;
+public class Ternary_operator_GreatestAmong3{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the 3 numbers: ");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int greater= (a>b && a>c)? a : (b>a && b>c) ? b : c;
+System.out.println(greater + " is the greatest number");
+}
+}
