df899d5fa3a6c90896b235adaa3d289bffdcf020
Avanti Kumari A <166237907+AvantiKumari-A@users.noreply.github.com>
2025-01-23 19:29:41 +0530

    Add files via upload

---
+++ Vowel_Consonant.java
@@ -0,0 +1,17 @@
+// Write a program to check the given char is vowel or not
+
+import java.util.Scanner;
+class Vowel_Consonant{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the character: ");
+char ch=sc.next().charAt(0);
+sc.close();
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+System.out.println(ch + " is a vowel");
+}
+else{
+System.out.println(ch + " is a consonant");
+}
+}
+}
