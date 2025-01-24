232a22453756a768988c24ac1bdbdd61ee7b77cd
Avanti Kumari A <166237907+AvantiKumari-A@users.noreply.github.com>
2025-01-24 22:46:09 +0530

    Update Float_Comparision.java

--- Float_Comparision.java
+++ Float_Comparision.java
@@ -7,6 +7,7 @@ Scanner sc=new Scanner(System.in);
 System.out.println("Enter the two values of float: ");
 float a=sc.nextFloat();
 float b=sc.nextFloat();
+sc.close();
 a=Math.round(a*1000)/1000;
 b=Math.round(b*1000)/1000;
 if(a==b){
