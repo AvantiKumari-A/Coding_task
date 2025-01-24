fd00b474d77a258d7dbab50911df18f8ad2e7918
Avanti Kumari A <166237907+AvantiKumari-A@users.noreply.github.com>
2025-01-24 23:12:12 +0530

    Update Add_twoNumbers.java

--- Add_twoNumbers.java
+++ Add_twoNumbers.java
@@ -1,10 +1,12 @@
-/*Imagine you have two randomly chosen numbers, and your goal is to write a program
+/* Imagine you have two randomly chosen numbers, and your goal is to write a program
 that adds them together.*/
 
 public class Add_twoNumbers{
 public static void main(String[] args){
 int a=68, b=54, sum;
-sum = a+ b;
-System.out.println("Sum of two numbers is: " + sum);
+sum = a + b;
+System.out.println("Value of a is: " + a);
+System.out.println("Value of b is: " + b);
+System.out.println("Sum of a + b is: " + sum);
 }
 }
