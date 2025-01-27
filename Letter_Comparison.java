ff1ced49f10bf4405e2749b32ab6c3a01514a0a7
Avanti Kumari A <166237907+AvantiKumari-A@users.noreply.github.com>
2025-01-27 23:19:32 +0530

    Update

--- Letter_Comparison.java
+++ Letter_Comparison.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class Letter_Comparison{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter the value of a and b: ");
+System.out.println("Enter the value of a (in char): ");
 char a=sc.next().charAt(0);
+System.out.println("Enter the value of a (in char): ");
 char b=sc.next().charAt(0);
 sc.close();
 System.out.println(a + " < " + b + " " + (a < b));
