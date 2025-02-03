d017e642874b4ad78a70eb3c8749fa5e58c244fc
Avanti Kumari A <166237907+AvantiKumari-A@users.noreply.github.com>
2025-02-03 21:21:11 +0530

    Add files via upload

---
+++ InfiniteLoop_for.java
@@ -0,0 +1,7 @@
+public class InfiniteLoop_for{
+public static void main(String[] args){
+for(;;){
+System.out.println("Looping forever");
+}
+}
+}
---
+++ InfiniteLoop_while.java
@@ -0,0 +1,7 @@
+public class InfiniteLoop_while{
+public static void main(String[] args){
+while(true){
+System.out.println("Looping forever");
+}
+}
+}
